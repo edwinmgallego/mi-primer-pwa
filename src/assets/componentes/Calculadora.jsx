a9c98ac344e7c6afdd371a77e35d195ea0553dc1
edwinmgallego <ing.edwin.gallego@gmail.com>
2024-10-08 20:43:25 -0500

    commit para que  comparen los estudiantes

---
+++ src/assets/componentes/Calculadora.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NumberImput from './NumberImput'
+
+const Calculadora = () => {
+  return (
+    <>
+    Calculadora -pwa
+    <NumberImput/>
+    </>
+  )
+}
+
+export default Calculadora
---
+++ src/assets/componentes/NumberImput.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { operaciones } from '../helpers/operaciones';
+import Resultado from './Resultado';
+
+const NumberImput = () => {
+
+    const[numeros,setNumeros]= useState({
+        numero1: 0,
+        numero2: 0
+    });
+    const {handleChange,suma,numero1,numero2} = operaciones(numeros,setNumeros)
+    
+  return (
+   <>NumberImput
+   <label htmlFor="">numero1: </label>
+   <input name='numero1' value={numero1} onChange={handleChange} type="number" />
+   <label htmlFor="">numero2: </label>
+   <input name='numero2' value={numero2} onChange={handleChange} type="number" />
+   <Resultado operacion="suma" calculo={suma()}/>
+   </>
+  )
+}
+
+NumberImput.propTypes = {
+    name: PropTypes.string
+}
+
+export default NumberImput
+
+
+
+
+
+
